0f0494ad4142142bcf27b891495a0f1daed608c8
Spotter1091 <adedayolamide.o@gmail.com>
2023-07-24 06:34:03 +0100

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import { React, useState } from "react";
+// import { Routes, Route, Link, Outlet } from "react-router-dom";
+import Pages from './Components/Pages/Pages'
+// import './index.css'
+
 
 export default function App() {
   return (
-    <div>App</div>
+    <Pages />
   )
 }
