51f4e1943a9180055306e57d4074ac441c1673a3
Spotter1091 <adedayolamide.o@gmail.com>
2023-07-24 06:28:16 +0100

    Update the Heder component

---
+++ src/Components/Common/Header/NavItems.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+export const navItems = [
+  {
+    id: 1,
+    title: "Home",
+    path: "./",
+    cName: "nav-item",
+  },
+  {
+    id: 2,
+    title: "Our Properties",
+    path: "./ourproperties",
+    cName: "nav-item",
+  },
+  {
+    id: 3,
+    title: "Schedule Inspection",
+    path: "./scheduleinspection",
+    cName: "nav-item",
+  },
+  {
+    id: 4,
+    title: "About Us",
+    path: "./aboutus",
+    cName: "nav-item",
+  },
+];
+
+export const serviceDropdown = [
+  {
+    id: 1,
+    title: "Ado",
+    path: "./ado",
+    cName: "submenu-item",
+  },
+  {
+    id: 2,
+    title: "Ikere",
+    path: "./ikere",
+    cName: "submenu-item",
+  },
+];
