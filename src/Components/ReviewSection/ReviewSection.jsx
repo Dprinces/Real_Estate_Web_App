dde134cf8112c924dfa0e9195df8ee8ca367bade
Spotter1091 <adedayolamide.o@gmail.com>
2023-07-25 13:59:02 +0100

    Create review section and Components

---
+++ src/Components/ReviewSection/ReviewSection.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+import "./ReviewSection.css";
+
+export default function () {
+  return (
+    <div className="review-wrapper">
+      <div className="review-title">
+        <h2>
+          Client<span className="span-title"> Reviews</span>
+        </h2>
+        <div className="border-bottom"></div>
+      </div>
+      <div className="review-container">
+        <div className="client-container">
+          <div className="client-pic">
+            <img src="/images/client1.png" alt="A couple's picture" />
+          </div>
+          <div className="client-info">
+            <h3>Mr & Mrs Olatunbosun</h3>
+          </div> 
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis ut
+            ducimus ullam praesentium reprehenderit debitis, voluptate autem eos
+            eligendi odio explicabo, quibusdam soluta similique quidem ratione
+            quis non, libero corporis qui suscipit. Esse, soluta laboriosam quam
+            culpa voluptatibus iusto quasi iure dolorem, eaque blanditiis
+            dignissimos amet pariatur sapiente natus consequuntur!
+          </p>
+        </div>
+        <div className="client-container">
+          <div className="client-pic">
+            <img src="" alt="" />
+          </div>
+          <div className="client-info">
+            <h3>Mr & Mrs Olatunbosun</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+              ut ducimus ullam praesentium reprehenderit debitis, voluptate
+              autem eos eligendi odio explicabo, quibusdam soluta similique
+              quidem ratione quis non, libero corporis qui suscipit. Esse,
+              soluta laboriosam quam culpa voluptatibus iusto quasi iure
+              dolorem, eaque blanditiis dignissimos amet pariatur sapiente natus
+              consequuntur!
+            </p>
+          </div>
+        </div>
+        <div className="client-container">
+          <div className="client-pic">
+            <img src="" alt="" />
+          </div>
+          <div className="client-info">
+            <h3>Mr & Mrs Olatunbosun</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+              ut ducimus ullam praesentium reprehenderit debitis, voluptate
+              autem eos eligendi odio explicabo, quibusdam soluta similique
+              quidem ratione quis non, libero corporis qui suscipit. Esse,
+              soluta laboriosam quam culpa voluptatibus iusto quasi iure
+              dolorem, eaque blanditiis dignissimos amet pariatur sapiente natus
+              consequuntur!
+            </p>
+          </div>
+        </div>
+        <div className="client-container">
+          <div className="client-pic">
+            <img src="" alt="" />
+          </div>
+          <div className="client-info">
+            <h3>Mr & Mrs Olatunbosun</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+              ut ducimus ullam praesentium reprehenderit debitis, voluptate
+              autem eos eligendi odio explicabo, quibusdam soluta similique
+              quidem ratione quis non, libero corporis qui suscipit. Esse,
+              soluta laboriosam quam culpa voluptatibus iusto quasi iure
+              dolorem, eaque blanditiis dignissimos amet pariatur sapiente natus
+              consequuntur!
+            </p>
+          </div>
+        </div>
+        <div className="client-container">
+          <div className="client-pic">
+            <img src="" alt="" />
+          </div>
+          <div className="client-info">
+            <h3>Mr & Mrs Olatunbosun</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+              ut ducimus ullam praesentium reprehenderit debitis, voluptate
+              autem eos eligendi odio explicabo, quibusdam soluta similique
+              quidem ratione quis non, libero corporis qui suscipit. Esse,
+              soluta laboriosam quam culpa voluptatibus iusto quasi iure
+              dolorem, eaque blanditiis dignissimos amet pariatur sapiente natus
+              consequuntur!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
