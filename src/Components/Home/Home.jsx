89aa7493a134039c87780b6ff584dfe94e5f67e3
Spotter1091 <adedayolamide.o@gmail.com>
2023-07-25 13:45:22 +0100

    add three components

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import Navbar from "../Common/Header/Navbar";
 import HeroSection from '../HeroSection/HerSection';
+import Features from "../Features/Features";
+import ReviewSection from '../ReviewSection/ReviewSection';
+
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <HeroSection />
+      <Features />
+      <ReviewSection />
     </>
   );
 }
