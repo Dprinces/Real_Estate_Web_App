3fe5c34720109bc9b1dae9859e5793476db5c473
Spotter1091 <adedayolamide.o@gmail.com>
2023-07-25 13:46:16 +0100

    Update Pages

--- src/Components/Pages/Pages.jsx
+++ src/Components/Pages/Pages.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Navbar from "../Common/Header/Navbar";
 import Home from '../Home/Home';
 
 export default function Pages() {
   return (
 <>
-    <Navbar />
+    
     <Home />
+    
     
 </>
   )
