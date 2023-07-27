ecc8063b27d0ad10dac59d3990b7e2f218ac5bd4
Spotter1091 <adedayolamide.o@gmail.com>
2023-07-27 09:44:57 +0100

    update file

--- src/Components/ScheduleInspection/ScheduleInspection.jsx
+++ src/Components/ScheduleInspection/ScheduleInspection.jsx
@@ -31,97 +31,99 @@ export default function ScheduleInspection() {
   return (
     <>
       <Navbar />
-      <div className="hero-section">
-        <h1>Schedule Inspection</h1>
-      </div>
-      <div className="form-container">
-        <div className="image-container">
-          <img src="../images/clip-art.png" alt="Clip Art" />
+      <div className="container">
+        <div className="hero-section">
+          <h1>Schedule Inspection</h1>
         </div>
-        <form onSubmit={handleSubmit}>
-          <label>
-            Name:
-            <input
-              type="text"
-              name="fullName"
-              value={formData.fullName}
-              onChange={handleChange}
-              placeholder="Enter your full name"
-              required
-            />
-          </label>
-          <label>
-            Email:
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              placeholder="Enter your email address"
-              required
-            />
-          </label>
-          <label>
-            Phone:
-            <input
-              type="tel"
-              name="phoneNumber"
-              value={formData.phoneNumber}
-              onChange={handleChange}
-              placeholder="Enter your phone number"
-              required
-            />
-          </label>
-          <label>
-            Type of Property:
-            <input
-              type="text"
-              name="typeOfProperty"
-              value={formData.typeOfProperty}
-              onChange={handleChange}
-              placeholder="Enter the type of property"
-              required
-            />
-          </label>
-          <label>
-            Select Preferred Contact Method:
-            <select
-              name="preferredContactMethod"
-              value={formData.preferredContactMethod}
-              onChange={handleChange}
-              required
-            >
-              <option value="" disabled>
-                Select an option
-              </option>
-              <option value="phone">Phone</option>
-              <option value="email">Email</option>
-            </select>
-          </label>
-          <label>
-            Inspection Date:
-            <input
-              type="date"
-              name="inspectionDate"
-              value={formData.inspectionDate}
-              onChange={handleChange}
-              placeholder="Select the inspection date"
-              required
-            />
-          </label>
-          <label>
-            Inspection Time:
-            <input
-              type="time"
-              name="inspectionTime"
-              value={formData.inspectionTime}
-              onChange={handleChange}
-              placeholder="Select the inspection time"
-              required
-            />
-          </label>
-          <button type="submit">Schedule Inspection</button>
-        </form>
+        <div className="form-container">
+          <div className="image-container">
+            <img src="../images/clip-art.png" alt="Clip Art" />
+          </div>
+          <form onSubmit={handleSubmit}>
+            <label>
+              Name:
+              <input
+                type="text"
+                name="fullName"
+                value={formData.fullName}
+                onChange={handleChange}
+                placeholder="Enter your full name"
+                required
+              />
+            </label>
+            <label>
+              Email:
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                placeholder="Enter your email address"
+                required
+              />
+            </label>
+            <label>
+              Phone:
+              <input
+                type="tel"
+                name="phoneNumber"
+                value={formData.phoneNumber}
+                onChange={handleChange}
+                placeholder="Enter your phone number"
+                required
+              />
+            </label>
+            <label>
+              Type of Property:
+              <input
+                type="text"
+                name="typeOfProperty"
+                value={formData.typeOfProperty}
+                onChange={handleChange}
+                placeholder="Enter the type of property"
+                required
+              />
+            </label>
+            <label>
+              Select Preferred Contact Method:
+              <select
+                name="preferredContactMethod"
+                value={formData.preferredContactMethod}
+                onChange={handleChange}
+                required
+              >
+                <option value="" disabled>
+                  Select an option
+                </option>
+                <option value="phone">Phone</option>
+                <option value="email">Email</option>
+              </select>
+            </label>
+            <label>
+              Inspection Date:
+              <input
+                type="date"
+                name="inspectionDate"
+                value={formData.inspectionDate}
+                onChange={handleChange}
+                placeholder="Select the inspection date"
+                required
+              />
+            </label>
+            <label>
+              Inspection Time:
+              <input
+                type="time"
+                name="inspectionTime"
+                value={formData.inspectionTime}
+                onChange={handleChange}
+                placeholder="Select the inspection time"
+                required
+              />
+            </label>
+            <button type="submit">Schedule Inspection</button>
+          </form>
+        </div>
       </div>
     </>
   );
