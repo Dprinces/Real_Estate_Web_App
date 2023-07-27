dce171369ec3c9b7e51da8db8521d94894511ae2
Spotter1091 <adedayolamide.o@gmail.com>
2023-07-27 10:32:50 +0100

    update file

--- src/Components/Common/Header/Button.jsx
+++ src/Components/Common/Header/Button.jsx
@@ -4,9 +4,16 @@ import "./Button.css";
 
 function Button() {
   return (
+    <div className="btn-wrapper">
     <Link to="signup">
+      <img
+        src="/images/profile-icon.png"
+        className="profile-icon"
+        alt="Profile icon"
+      />
       <button className="btn">My Property</button>
     </Link>
+    </div>
   );
 }
 
