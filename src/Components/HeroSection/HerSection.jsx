c9d81071d25e06010170bff1671c4475e825bd8a
Spotter1091 <adedayolamide.o@gmail.com>
2023-07-29 04:11:45 +0100

    Update file

--- src/Components/HeroSection/HerSection.jsx
+++ src/Components/HeroSection/HerSection.jsx
@@ -1,25 +1,30 @@
 import React from "react";
+import Stats from "../Stats/Stats";
 import "./HeroSection.css";
 
 export default function HerSection() {
   return (
-    <div className="hero-container">
-      <div className="content-wrapper">
-        <h1 className="hero-title">
-          Why rent?
-          <br />
-          When you can own.
-        </h1>
-        <p class="hero-text">
-          Owning a home is not just about four walls, <br /> it's about building
-          memories.
-        </p>
-        <div className="search-bar">
-          <input type="text" placeholder="You are one search away..." />
-          <button className="search-button">SEARCH</button>
+    <>
+      <div className="hero-container">
+        <div className="content-wrapper">
+          <h1 className="hero-title">
+            Why rent?
+            <br />
+            When you can own.
+          </h1>
+          <p class="hero-text">
+            Owning a home is not just about four walls, <br /> it's about
+            building memories.
+          </p>
+          <div className="search-bar">
+            <input type="text" placeholder="You are one search away..." />
+            <button className="search-button">SEARCH</button>
+          </div>
         </div>
       </div>
-    </div>
+
+      <Stats />
+    </>
   );
 }
 
