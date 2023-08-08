c89b6ec1ba855d1640e5bd48255d40a3a4ff076a
Spotter1091 <adedayolamide.o@gmail.com>
2023-08-08 13:04:19 +0100

    update file

--- src/Components/ReviewSection/Data/Reviews.js
+++ src/Components/ReviewSection/Data/Reviews.js
@@ -8,7 +8,7 @@ export const reviews = [
   },
   {
     id: "2",
-    img: "/images/client1.png",
+    img: "/images/client2.png",
     reviewerName: "Richie Black",
     review:
       "Thank you for the exceptional service and support throughout my real estate journey. Your professionalism, expertise, and personalized approach made the entire process smooth and enjoyable. I am grateful for your commitment to excellence in helping me find my dream home. You truly exceeded my expectations.",
@@ -17,14 +17,14 @@ export const reviews = [
   },
   {
     id: "3",
-    img: "/images/client1.png",
+    img: "/images/client3.png",
     reviewerName: "Mr. Adigun Alapomeji",
     review:
       "Thank you for the exceptional service and support throughout my real estate journey. Your professionalism, expertise, and personalized approach made the entire process smooth and enjoyable. I am grateful for your commitment to excellence in helping me find my dream home. You truly exceeded my expectations."
   },
   {
     id: "4",
-    img: "/images/client1.png",
+    img: "/images/client4.png",
     reviewerName: "Mrs. Beatrice Ajayi",
     review:
       "Thank you for the exceptional service and support throughout my real estate journey. Your professionalism, expertise, and personalized approach made the entire process smooth and enjoyable. I am grateful for your commitment to excellence in helping me find my dream home. You truly exceeded my expectations.",
