355a1fd4c0ca435166ec3c66f9554cfdf5883c86
Spotter1091 <adedayolamide.o@gmail.com>
2024-01-18 14:02:33 +0100

    update file

--- src/Components/Features/House/House.jsx
+++ src/Components/Features/House/House.jsx
@@ -67,219 +67,299 @@ export default function House() {
       </div>
       <div className="bedroom-ter-wrapper">
         <div>
-          <img src="../../../../images/HouseDes1.png" alt="Location icon" />
+          <img
+            src="../../../../images/HouseDes1.png"
+            className="bedroom-building"
+            alt="A duplex"
+          />
         </div>
 
         <div className="description1">
-          <h1> 4 Bedroom Terrace</h1>
-          <ul>
+          <div className="description-heading">
+            <h1> 4 Bedroom Terrace</h1>
+          </div>
+          <ul className="checkbox-list">
             <li>
-              Fitted Kitchen cabinets and counter tops, refrigerator, cooker,
-              hob and microwave
+              <input
+                type="checkbox"
+                id="kitchen"
+                name="feature"
+                value="Fitted Kitchen cabinets and counter tops, refrigerator, cooker, hob, and microwave"
+                class="custom-checkbox"
+              />
+              <label for="kitchen" class="custom-checkbox-label">
+                Fitted Kitchen cabinets and counter tops, refrigerator, cooker,
+                hob, and microwave
+              </label>
             </li>
-            <li>Balconies wherever applicable as per unit plan</li>
-            <li>Wardrobes in the bedroom</li>
-            <li>Fully tiled bathrooms ensuite and guest toilet</li>
-            <li>Electrical shaver point with mirror in bathroom</li>
-            <li>Water Heater</li>
-            <li>Tall windows</li>
+
+            <li>
+              <input
+                type="checkbox"
+                id="balconies"
+                name="feature"
+                value="Balconies wherever applicable as per unit plan"
+                class="custom-checkbox"
+              />
+              <label for="balconies">
+                Balconies wherever applicable as per unit plan
+              </label>
+            </li>
+
+            <li>
+              <input
+                type="checkbox"
+                id="wardrobes"
+                name="feature"
+                value="Wardrobes in the bedroom"
+                class="custom-checkbox"
+              />
+              <label for="wardrobes">Wardrobes in the bedroom</label>
+            </li>
+
+            <li>
+              <input
+                type="checkbox"
+                id="bathrooms"
+                name="feature"
+                value="Fully tiled bathrooms ensuite and guest toilet"
+                class="custom-checkbox"
+              />
+              <label for="bathrooms">
+                Fully tiled bathrooms ensuite and guest toilet
+              </label>
+            </li>
+
+            <li>
+              <input
+                type="checkbox"
+                id="shaver"
+                name="feature"
+                value="Electrical shaver point with mirror in bathroom"
+                class="custom-checkbox"
+              />
+              <label for="shaver" class="custom-checkbox-label">
+                Electrical shaver point with mirror in bathroom
+              </label>
+            </li>
+
+            <li>
+              <input
+                type="checkbox"
+                id="heater"
+                name="feature"
+                value="Water Heater"
+                class="custom-checkbox"
+              />
+              <label for="heater">Water Heater</label>
+            </li>
+
+            <li>
+              <input
+                type="checkbox"
+                id="windows"
+                name="feature"
+                value="Tall windows"
+                class="custom-checkbox"
+              />
+              <label for="windows">Tall windows</label>
+            </li>
           </ul>
         </div>
-        </div>
-        <h1>
-          Price: N80M <br /> Type: Service Estate
-        </h1>
-        <section className="bedroom-display">
-          <div>
-            <img src="../../../../images/Bedroom1.png" alt="Location icon" />
-          </div>
-          <div>
-            <img src="../../../../images/Bedroom2.png" alt="Location icon" />
-          </div>
-          <div>
-            <img src="../../../../images/Bedroom3.png" alt="Location icon" />
-          </div>
-          <div>
-            <img src="../../../../images/Bedroom4.png" alt="Location icon" />
-          </div>
-          <div>
-            <img src="../../../../images/Bedroom5.png" alt="Location icon" />
-          </div>
-          <div>
-            <img src="../../../../images/Bedroom6.png" alt="Location icon" />
-          </div>
-          <div>
-            <img src="../../../../images/Bedroom7.png" alt="Location icon" />
-          </div>
-          <div>
-            <img src="../../../../images/Bedroom8.png" alt="Location icon" />
-          </div>
-          <div>
-            <img src="../../../../images/Bedroom9.png" alt="Location icon" />
-          </div>
-        </section>
-        <div className="p-wrapper">
-          <div className="p-payment-title">
-            <h3>Our Payment Plan</h3>
-          </div>
-          <div className="payment-plan-container">
-            <div className="payment-plan">
-              <button>Outright</button>
-              <button>6 Months</button>
-              <button>12 Months</button>
-              <button>18 Months</button>
-              <button>24 Months</button>
-              <button>38 Months</button>
-            </div>
-          </div>
-        </div>
-
+      </div>
+      <h1>
+        Price: N80M <br /> Type: Service Estate
+      </h1>
+      <section className="bedroom-display">
         <div>
-          <img src="../../../../images/HouseDes2.png" alt="Location icon" />
+          <img src="../../../../images/Bedroom1.png" alt="Location icon" />
         </div>
-        <h1> 3 Bedroom Terrace</h1>
-        <ul>
-          <li>
-            Fitted Kitchen cabinets and counter tops, refrigerator, cooker, hob
-            and microwave
-          </li>
-          <li>Balconies wherever applicable as per unit plan</li>
-          <li>Wardrobes in the bedroom</li>
-          <li>Fully tiled bathrooms ensuite and guest toilet</li>
-          <li>Electrical shaver point with mirror in bathroom</li>
-          <li>Water Heater</li>
-          <li>Tall windows</li>
-        </ul>
-        <h1>
-          Price: N80M <br /> Type: Service Estate
-        </h1>
-        <section className="bedroom-display">
-          <div>
-            <img src="../../../../images/Bedroom10.png" alt="Location icon" />
-          </div>
-          <div>
-            <img src="../../../../images/Bedroom11.png" alt="Location icon" />
-          </div>
-          <div>
-            <img src="../../../../images/Bedroom12.png" alt="Location icon" />
-          </div>
-          <div>
-            <img src="../../../../images/Bedroom13.png" alt="Location icon" />
-          </div>
-          <div>
-            <img src="../../../../images/Bedroom14.png" alt="Location icon" />
-          </div>
-          <div>
-            <img src="../../../../images/Bedroom15.png" alt="Location icon" />
-          </div>
-          <div>
-            <img src="../../../../images/Bedroom16.png" alt="Location icon" />
-          </div>
-          <div>
-            <img src="../../../../images/Bedroom17.png" alt="Location icon" />
-          </div>
-          <div>
-            <img src="../../../../images/Bedroom18.png" alt="Location icon" />
-          </div>
-          <div>
-            <img src="../../../../images/Bedroom19.png" alt="Location icon" />
-          </div>
-        </section>
-        <div className="p-wrapper">
-          <div className="p-payment-title">
-            <h3>Our Payment Plan</h3>
-          </div>
-          <div className="payment-plan-container">
-            <div className="payment-plan">
-              <button>Outright</button>
-              <button>6 Months</button>
-              <button>12 Months</button>
-              <button>18 Months</button>
-              <button>24 Months</button>
-              <button>38 Months</button>
-            </div>
+        <div>
+          <img src="../../../../images/Bedroom2.png" alt="Location icon" />
+        </div>
+        <div>
+          <img src="../../../../images/Bedroom3.png" alt="Location icon" />
+        </div>
+        <div>
+          <img src="../../../../images/Bedroom4.png" alt="Location icon" />
+        </div>
+        <div>
+          <img src="../../../../images/Bedroom5.png" alt="Location icon" />
+        </div>
+        <div>
+          <img src="../../../../images/Bedroom6.png" alt="Location icon" />
+        </div>
+        <div>
+          <img src="../../../../images/Bedroom7.png" alt="Location icon" />
+        </div>
+        <div>
+          <img src="../../../../images/Bedroom8.png" alt="Location icon" />
+        </div>
+        <div>
+          <img src="../../../../images/Bedroom9.png" alt="Location icon" />
+        </div>
+      </section>
+      <div className="p-wrapper">
+        <div className="p-payment-title">
+          <h3>Our Payment Plan</h3>
+        </div>
+        <div className="payment-plan-container">
+          <div className="payment-plan">
+            <button>Outright</button>
+            <button>6 Months</button>
+            <button>12 Months</button>
+            <button>18 Months</button>
+            <button>24 Months</button>
+            <button>38 Months</button>
           </div>
         </div>
+      </div>
 
-        <section>
-          <p>Register Your Interest Today</p>
-          <form onSubmit={handleSubmit}>
-            <label>Name</label>
-            <input
-              type="text"
-              name="Name"
-              value={formData.Name}
-              onChange={handleChange}
-              placeholder="Enter your full name"
-              required
-            />
-
-            <label>Email</label>
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              placeholder="Enter your email address"
-              required
-            />
-
-            <label>Tel.</label>
-            <input
-              type="tel"
-              name="tel"
-              value={formData.tel}
-              onChange={handleChange}
-              placeholder="Enter your phone number"
-              required
-            />
-
-            <label>Payment Plans</label>
-            <select
-              name="paymentPlans"
-              value={formData.paymentPlans}
-              onChange={handleChange}
-              required
-              className="select-one"
-            >
-              <option value="" disabled>
-                Payment Plans
-              </option>
-              <option value="outright">Outright</option>
-              <option value="6months">6 Months</option>
-              <option value="12months">12 Months</option>
-              <option value="18months">18 Months</option>
-              <option value="24months">24 Months</option>
-              <option value="36months">36 Months</option>
-            </select>
-
-            <label>Select a Preferred Contact Method</label>
-            <select
-              name="preferredContactMethod"
-              value={formData.preferredContactMethod}
-              onChange={handleChange}
-              className="select-one"
-              required
-            >
-              <option value="" disabled>
-                Select an option
-              </option>
-              <option value="phone">Phone</option>
-              <option value="email">Email</option>
-            </select>
-
-            <button type="submit">Submit</button>
-          </form>
-          <div className="quick-r-btn">
-            <img
-              src="/icons/whatsapp.png"
-              alt="whatsapp"
-              className="whatsapp-icon"
-            />
-            <button> Quick Response</button>
+      <div>
+        <img src="../../../../images/HouseDes2.png" alt="Location icon" />
+      </div>
+      <h1> 3 Bedroom Terrace</h1>
+      <ul>
+        <li>
+          Fitted Kitchen cabinets and counter tops, refrigerator, cooker, hob
+          and microwave
+        </li>
+        <li>Balconies wherever applicable as per unit plan</li>
+        <li>Wardrobes in the bedroom</li>
+        <li>Fully tiled bathrooms ensuite and guest toilet</li>
+        <li>Electrical shaver point with mirror in bathroom</li>
+        <li>Water Heater</li>
+        <li>Tall windows</li>
+      </ul>
+      <h1>
+        Price: N80M <br /> Type: Service Estate
+      </h1>
+      <section className="bedroom-display">
+        <div>
+          <img src="../../../../images/Bedroom10.png" alt="Location icon" />
+        </div>
+        <div>
+          <img src="../../../../images/Bedroom11.png" alt="Location icon" />
+        </div>
+        <div>
+          <img src="../../../../images/Bedroom12.png" alt="Location icon" />
+        </div>
+        <div>
+          <img src="../../../../images/Bedroom13.png" alt="Location icon" />
+        </div>
+        <div>
+          <img src="../../../../images/Bedroom14.png" alt="Location icon" />
+        </div>
+        <div>
+          <img src="../../../../images/Bedroom15.png" alt="Location icon" />
+        </div>
+        <div>
+          <img src="../../../../images/Bedroom16.png" alt="Location icon" />
+        </div>
+        <div>
+          <img src="../../../../images/Bedroom17.png" alt="Location icon" />
+        </div>
+        <div>
+          <img src="../../../../images/Bedroom18.png" alt="Location icon" />
+        </div>
+        <div>
+          <img src="../../../../images/Bedroom19.png" alt="Location icon" />
+        </div>
+      </section>
+      <div className="p-wrapper">
+        <div className="p-payment-title">
+          <h3>Our Payment Plan</h3>
+        </div>
+        <div className="payment-plan-container">
+          <div className="payment-plan">
+            <button>Outright</button>
+            <button>6 Months</button>
+            <button>12 Months</button>
+            <button>18 Months</button>
+            <button>24 Months</button>
+            <button>38 Months</button>
           </div>
-        </section>
-      
+        </div>
+      </div>
+
+      <section>
+        <p>Register Your Interest Today</p>
+        <form onSubmit={handleSubmit}>
+          <label>Name</label>
+          <input
+            type="text"
+            name="Name"
+            value={formData.Name}
+            onChange={handleChange}
+            placeholder="Enter your full name"
+            required
+          />
+
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder="Enter your email address"
+            required
+          />
+
+          <label>Tel.</label>
+          <input
+            type="tel"
+            name="tel"
+            value={formData.tel}
+            onChange={handleChange}
+            placeholder="Enter your phone number"
+            required
+          />
+
+          <label>Payment Plans</label>
+          <select
+            name="paymentPlans"
+            value={formData.paymentPlans}
+            onChange={handleChange}
+            required
+            className="select-one"
+          >
+            <option value="" disabled>
+              Payment Plans
+            </option>
+            <option value="outright">Outright</option>
+            <option value="6months">6 Months</option>
+            <option value="12months">12 Months</option>
+            <option value="18months">18 Months</option>
+            <option value="24months">24 Months</option>
+            <option value="36months">36 Months</option>
+          </select>
+
+          <label>Select a Preferred Contact Method</label>
+          <select
+            name="preferredContactMethod"
+            value={formData.preferredContactMethod}
+            onChange={handleChange}
+            className="select-one"
+            required
+          >
+            <option value="" disabled>
+              Select an option
+            </option>
+            <option value="phone">Phone</option>
+            <option value="email">Email</option>
+          </select>
+
+          <button type="submit">Submit</button>
+        </form>
+        <div className="quick-r-btn">
+          <img
+            src="/icons/whatsapp.png"
+            alt="whatsapp"
+            // className="whatsapp-icon"
+          />
+          <button> Quick Response</button>
+        </div>
+      </section>
 
       <Footer />
     </>
--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../Components/Common/Header/Navbar";
 import Footer from "../Components/Common/Footer/Footer";
 import { Link } from "react-router-dom";
-import './AboutUs.css'
+import "./AboutUs.css";
 
 export default function AboutUs() {
   return (
@@ -10,62 +10,104 @@ export default function AboutUs() {
       <Navbar />
       <div className="aboutus-container">
         <div>
-          <img src="/images/aboutus-hero.png" alt="Location icon" />
+          <img
+            src="/images/aboutus-hero.png"
+            alt="Location icon"
+            className="hero-image"
+          />
         </div>
         <div className="para-box">
           <p className="aboutus-para">
             Home of Honour is a trusted and award winning real estate
             development company operating out of Ekiti Nigeria, with over 5 real
-            estate products across the Ekiti Metropolitan Cities. <br />With 50
-            trained independent marketers, we have been able to help over 159
-            Nigerians become property owners of over over one 1000 square metres
-            of well documented and titled landed properties within the space of
-            4 years.
+            estate products across the Ekiti Metropolitan Cities. <br />
+            With 50 trained independent marketers, we have been able to help
+            over 159 Nigerians become property owners of over over one 1000
+            square metres of well documented and titled landed properties within
+            the space of 4 years.
             <br />
             Our Success story is not far-fetched: we are diligent and we keep
-            our word. < br/>Our focus is to bring clarity to, and demystify the
-            process of buying and selling real estate in Nigeria: especially in
-            Ekiti State. All our Properties are free from Government
-            acquisitions, land-grabbers trouble or any other encumbrance
-            whatsoever. <br />We offer other value added services including but not
-            limited to legal advice on property matters, architectural drawings
-            etc. <br/>Under the leadership of our amiable chairman, Chief Bosun
-            Makinde, our mission is to render top-notch services to our numerous
+            our word. <br />
+            Our focus is to bring clarity to, and demystify the process of
+            buying and selling real estate in Nigeria: especially in Ekiti
+            State. All our Properties are free from Government acquisitions,
+            land-grabbers trouble or any other encumbrance whatsoever. <br />
+            We offer other value added services including but not limited to
+            legal advice on property matters, architectural drawings etc. <br />
+            Under the leadership of our amiable chairman, Chief Bosun Makinde,
+            our mission is to render top-notch services to our numerous
             satisfied clients while maintaining the highest standards of
             integrity without stress or hassel
           </p>
         </div>
+
         <section className="award-container">
-          <h2 className="award-title">AWARDS</h2>
-        </section>
-        <section className="grid-container">
-          <div className="grid-one">
-            <p>
-              Our major aim is to provide you an affordable properties without
-              having to break the bank.
+          <div className="award-title-container flex">
+            <h2 className="award-title">AWARDS</h2>
+            <p className="award-sub-txt">
+              Among several others, we have been honored with numerous awards
+              such as:
             </p>
           </div>
-          <div className="awardicon">
-            <img
-              src="/icons/dashicons_awards.svg"
-              alt="dashicons_awards"
-              className="dashicons_awards"
-            />
-            <img
-              src="/icons/dashicons_awards.svg"
-              alt="dashicons_awards"
-              className="dashicons_awards"
-            />
-            <img
-              src="/icons/dashicons_awards.svg"
-              alt="dashicons_awards"
-              className="dashicons_awards"
-            />
+          <div className="grid-container">
+            <div className="grids">
+              <div className="box-flex">
+                <div className="award-container">
+                  <div className="dashicon-one">
+                    <img
+                      src="/icons/dashicons_awards.svg"
+                      className="dashicon-one-img center-circle"
+                      alt="Award icon"
+                    />
+                  </div>
+                  <div className="box">
+                    <p className="box-text">
+                      real estate excellence awards nomination (2021).
+                    </p>
+                  </div>
+                </div>
+
+                <div>
+                  <div className="dashicon-one">
+                    <img
+                      src="/icons/dashicons_awards.svg"
+                      className="dashicon-one-img center-circle"
+                      alt="Award icon"
+                    />
+                  </div>
+
+                  <div className="box">
+                    <p className="box-text">
+                      West Africa Innovation Awards (Most Customer Driven
+                      Property Development Company Of The Year).
+                    </p>
+                  </div>
+                </div>
+
+                <div>
+                  <div className="dashicon-one">
+                    <img
+                      src="/icons/dashicons_awards.svg"
+                      className="dashicon-one-img center-circle"
+                      alt="Award icon"
+                    />
+                  </div>
+
+                  <div className="box">
+                    <p className="box-text">
+                      The Nigerian Real Estate And Property Award (Real Estate
+                      Company Of The Year, 2022).
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
-        <section className="mission">
+
+        <section className="mission-vision">
           <img
-            src="/images/Mission-img.png"
+            src="/images/boardroom.png"
             alt="Mission & vision"
             className="mission-img"
           />
